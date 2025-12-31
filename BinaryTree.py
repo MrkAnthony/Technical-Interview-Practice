f877c80721a9a0d3fa742375e80c693573aff498
Mark-Anthony Delva <mdelv065@fiu.edu>
2025-12-31 08:40:56 -0500

    BST Tree Practice

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,39 +1,60 @@
 from collections import deque
 
+
 class TreeNode:
     def __init__(self, value=0, left=None, right=None):
         self.val = value
         self.left = left
         self.right = right
 
-def level_order(root):
-    # If the tree is empty:
-    # return an empty list
+
+def check_tree(root):
     if not root:
-        return []
-    # Create an empty queue using deque
-    # Create an empty list to store the explored nodes
-    queue = deque([root])
-    result = []
+        return False
 
-    # Add the root to the queue
-    # While the queue is not empty:
-    while queue:
-        node_popped = queue.popleft()
-        #print(node_popped)
-        # Pop the next node off the queue (pop from the left side!)
-        # Add the popped node to the list of explored nodes
-        result.append(root.val)
+    res = 0
+    if root.left and root.right:
+        res += (root.left.val + root.right.val)
+    elif root.left and not root.right:
+        res += root.left.val
+    elif root.right and not root.left:
+        res += root.right.val
+    return res == root.val
 
-        # Add each of the popped node's children to the end of the queue
-        queue.append(node_popped)
-    # Return the list of visited nodes
-    return result
 
-#root = TreeNode(4, TreeNode(2, TreeNode(1,TreeNode(6)), TreeNode(3))) # Expected [4, 2, 6, 1, 3]
-root = TreeNode(4)
-root.left = TreeNode(2)
-root.left.left = TreeNode(1)
-root.left.right = TreeNode(3)
-root.right = TreeNode(6)
-print(level_order(root))
+def left_most(root):
+    if not root:
+        return 0
+    if not root.left:
+        return 0
+
+    deepest_node = 0
+    while root.left:
+        deepest_node = root.left.val
+        root.left = root.left.left
+
+    return deepest_node
+
+
+'''
+UNDERSTAND
+- Given the root of a binary tree, return a list representing the inorder traversal of it's nodes values.
+- In an inorder traversal we traverse the left subtree, then the current node, then the right subtree.
+INPUT: is the root of a binary tree
+OUTPUT: a lst of nodes that show the traversal of inorder traversal
+'''
+
+
+def inorder_traversal(root) -> list[int]:
+    pass
+
+
+root = TreeNode(10)
+root.left = TreeNode(5)
+root.left.left = TreeNode(3)
+root.left.right = TreeNode(6)
+root.right = TreeNode(15)
+root.right.left = TreeNode(12)
+root.right.right = TreeNode(17)
+
+print(inorder_traversal(root))
